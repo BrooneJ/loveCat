d7707fbc88e47bc4cf5ce431ed58054fd449d1c6
HyungwonJin <86827200+HyungwonJin@users.noreply.github.com>
2021-10-06 23:03:55 +0900

    fix: theCatAPI.js 원래 endpoint에서 load할 때 error가 발생하므로 다른 endpoint로 변경

--- src/api/theCatAPI.js
+++ src/api/theCatAPI.js
@@ -1,5 +1,4 @@
-const API_ENDPOINT =
-    "https://oivhcpn8r9.execute-api.ap-northeast-2.amazonaws.com/dev";
+const API_ENDPOINT = 'https://api.thecatapi.com/v1';
 
 const request = async url => {
     try {
@@ -12,28 +11,28 @@ const request = async url => {
             throw errorData;
         }
     } catch (e) {
-        console.log(e)
-        // throw {
-        //     message: e.message,
-        //     status: e.status
-        // };
+        throw {
+            message: e.message,
+            status: e.status
+        };
     }
-}
+};
 
 const api = {
     fetchCats: async keyword => {
+        /*
+            keyword로 breed를 찾고 각 breed의 id로 이미지를 찾는다.
+        */
         try {
-            const catLists = await request(`${API_ENDPOINT}/api/cats/search?q=${keyword}`);
-            const requests = catLists.data.map(async catList => {
-                return await request(`${API_ENDPOINT}/api/cats/${catList.id}`);
+            const breeds = await request(`${API_ENDPOINT}/breeds/search?q=${keyword}`);
+            console.log(breeds);
+            const requests = breeds.map(async breed => {
+                return await request(`${API_ENDPOINT}/images/search?limit=20&breed_ids=${breed.id}`);
             });
-
+            console.log(requests);
             const responses = await Promise.all(requests);
-
-            console.log('responses:', responses);
-            const filterObj = responses.filter(el => el !== undefined);
-            const result = Array.prototype.concat.apply([], filterObj);
-
+            const result = Array.prototype.concat.apply([], responses);
+            console.log(result)
             return {
                 isError: false,
                 data: result
@@ -42,22 +41,15 @@ const api = {
             return {
                 isError: true,
                 data: e
-            }
+            };
         }
     },
-
     fetchRandomCats: async () => {
+        /*
+            랜덤으로 20개의 고양이 사진을 리턴한다.
+        */
         try {
-            const randoms = await request(`${API_ENDPOINT}/api/cats/random50`);
-            const requests = randoms.data.map(async catList => {
-                return await request(`${API_ENDPOINT}/api/cats/${catList.id}`);
-            });
-
-            const responses = await Promise.all(requests);
-
-            console.log('responses:', responses);
-            const filterObj = responses.filter(el => el !== undefined);
-            const result = Array.prototype.concat.apply([], filterObj);
+            const result = await request(`${API_ENDPOINT}/images/search?limit=20`);
             return {
                 isError: false,
                 data: result
@@ -72,3 +64,78 @@ const api = {
 };
 
 export { api };
+// const API_ENDPOINT =
+//     "https://oivhcpn8r9.execute-api.ap-northeast-2.amazonaws.com/dev";
+// const request = async url => {
+//     try {
+//         const response = await fetch(url);
+//         if (response.ok) {
+//             const data = await response.json();
+//             return data;
+//         } else {
+//             const errorData = await response.json();
+//             throw errorData;
+//         }
+//     } catch (e) {
+//         console.log(e)
+//         // throw {
+//         //     message: e.message,
+//         //     status: e.status
+//         // };
+//     }
+// }
+
+// const api = {
+//     fetchCats: async keyword => {
+//         try {
+//             const catLists = await request(`${API_ENDPOINT}/api/cats/search?q=${keyword}`);
+//             const requests = catLists.data.map(async catList => {
+//                 return await request(`${API_ENDPOINT}/api/cats/${catList.id}`);
+//             });
+//             const responses = await Promise.all(requests);
+
+//             console.log('responses:', responses);
+//             const filterObj = responses.filter(el => el !== undefined);
+//             const tmp = filterObj.map(el => el.data);
+//             console.log('tmp', tmp);
+//             // const result = Array.prototype.concat.apply([], filterObj);
+//             // console.log('result:', result)
+//             return {
+//                 isError: false,
+//                 data: tmp
+//             };
+//         } catch (e) {
+//             return {
+//                 isError: true,
+//                 data: e
+//             }
+//         }
+//     },
+
+//     fetchRandomCats: async () => {
+//         try {
+//             const randoms = await request(`${API_ENDPOINT}/api/cats/random50`);
+//             const requests = randoms.data.map(async catList => {
+//                 return await request(`${API_ENDPOINT}/api/cats/${catList.id}`);
+//             });
+
+//             const responses = await Promise.all(requests);
+//             const tmp = [];
+//             console.log('responses:', responses);
+//             responses.then(el => tmp.push(el))
+//             console.log('responses:', tmp);
+//             // const filterObj = responses.filter(el => el !== undefined);
+//             const result = Array.prototype.concat.apply([], filterObj);
+//             return {
+//                 isError: false,
+//                 data: result
+//             };
+//         } catch (e) {
+//             return {
+//                 isError: true,
+//                 data: e
+//             };
+//         }
+//     }
+// };
+// export { api };
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,19 @@
 export default class Card {
     constructor({ $target, data }) {
-        this.data = data.data;
+        this.data = data;
         this.card = document.createElement('article');
         this.card.className = 'cat-card';
         this.card.dataset.id = data.id;
 
         $target.appendChild(this.card);
-
+        // console.log(this.data);
         this.render();
     }
 
     render() {
         const url = this.data.url;
-        const { name, origin } = this.data;
+        // const { name, origin } = this.data;
+        const { name, origin } = this.data.breeds.length > 0 ? this.data.breeds[0] : { name: '정보없음', origin: '정보없음' };
 
         const cardImage = document.createElement('img');
         cardImage.className = 'card-image';
